c1c230113a7717b0e7566fc9d34acf0c04f18bdb
shaikzahid405 <shaikzahid405@gmail.com>
2020-04-08 12:03:37 +0530

    Initial Update

--- src/main/java/com/example/jokeapp/JokeappApplication.java
+++ src/main/java/com/example/jokeapp/JokeappApplication.java
@@ -2,8 +2,10 @@ package com.example.jokeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example"})
 public class JokeappApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/jokeapp/services/JokeService.java
@@ -0,0 +1,5 @@
+package com.example.jokeapp.services;
+
+public interface JokeService {
+    String getJoke();
+}
